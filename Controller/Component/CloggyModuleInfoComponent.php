73f713e5bee070869db8c45f4d9ef851acc626b6
Hiraq <hrxwan@gmail.com>
2012-12-30 22:38:22 +0700

    cloggy module info component unit test & docs

--- Controller/Component/CloggyModuleInfoComponent.php
+++ Controller/Component/CloggyModuleInfoComponent.php
@@ -2,69 +2,113 @@
 
 App::uses('Component', 'Controller');
 
+/**
+ * Manage modules including their info.ini
+ * 
+ * @author hiraq
+ * @name CloggyModuleInfoComponent
+ * @package Cloggy
+ * @subpackage Component
+ */
 class CloggyModuleInfoComponent extends Component {
 		
-	private $_modules = array();	
+/**
+ * Store registered modules
+ * 
+ * @access private
+ * @var array
+ */	
+	private $__modules = array();	
 	
-	public function modules() {
-		
+/**
+ * Setup and get all registered modules
+ * @access public
+ */	
+	public function modules() {		
 		$modules = Configure::read('Cloggy.modules');
-		if(!empty($modules) && is_array($modules)) {
-			
+		if(!empty($modules) && is_array($modules)) {			
 			foreach($modules as $module) {
-				if(!array_key_exists($module,$this->_modules)) {
-					
+				if (!array_key_exists($module,$this->__modules)) {					
 					$this->_configureModuleInfo($module);
-					$this->_modules[$module]['name'] = $this->getModuleName();
-					$this->_modules[$module]['desc'] = $this->getModuleDesc();
-					$this->_modules[$module]['author'] = $this->getModuleAuthor();
-					
+					$this->__modules[$module]['name'] = $this->getModuleName();
+					$this->__modules[$module]['desc'] = $this->getModuleDesc();
+					$this->__modules[$module]['author'] = $this->getModuleAuthor();					
 				}
-			}
-			
-		}
-		
+			}			
+		}		
 	}
 	
+/**
+ * Get all modules
+ * @access public
+ */	
 	public function getModules() {
-		return $this->_modules;
+		return $this->__modules;
 	}
 	
+/**
+ * Check if module exists or not
+ * @access public
+ * @param string $module
+ * @return boolean
+ */	
 	public function isModuleExists($module) {
-		return array_key_exists($module,$this->_modules);
+		return array_key_exists($module,$this->__modules);
 	}
 	
-	public function getModuleInfo($module) {
-		
-		if(array_key_exists($module,$this->_modules)) {			
-			return $this->_modules[$module];			
-		}else{
+/**
+ * Get module info
+ * @access public
+ * @param string $module
+ * @return null|array
+ */	
+	public function getModuleInfo($module) {		
+		if (array_key_exists($module,$this->__modules)) {			
+			return $this->__modules[$module];			
+		} else {
 			return null;
-		}
-		
+		}		
 	}
 	
+/**
+ * Get module name
+ * @access public
+ * @return string
+ */	
 	public function getModuleName() {
 		$name = Configure::read('basic.name');
 		return $name;
 	}		
 	
+/**
+ * Get module description
+ * @access public
+ * @return string
+ */	
 	public function getModuleDesc() {
 		$desc = Configure::read('basic.desc');
 		return $desc;
 	}
 	
+/**
+ * Get module author
+ * @access public
+ * @return string
+ */	
 	public function getModuleAuthor() {
 		$author = Configure::read('basic.author');
 		return $author;
 	}
 	
-	private function _configureModuleInfo($moduleName) {
-		
+/**
+ * Load info.ini for requested module
+ * @access private
+ * @param string $moduleName
+ */	
+	private function _configureModuleInfo($moduleName) {		
 		App::uses('IniReader', 'Configure');
 		Configure::config('ini',new IniReader(CLOGGY_PATH_MODULE.$moduleName.DS));
-		Configure::load('info.ini','ini');
-		
+		Configure::load('info.ini','ini');		
 	}
 	
 }
