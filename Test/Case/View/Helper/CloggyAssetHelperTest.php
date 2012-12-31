0205911fae66dff536945aa90eb449f05e810998
Hiraq <hrxwan@gmail.com>
2012-12-31 17:29:50 +0700

    cloggy asset helper & docs

---
+++ Test/Case/View/Helper/CloggyAssetHelperTest.php
@@ -0,0 +1,65 @@
+<?php
+
+App::uses('Controller', 'Controller');
+App::uses('CakeRequest', 'Network');
+App::uses('CakeResponse', 'Network');
+App::uses('View', 'View');
+App::uses('CloggyAssetHelper', 'Cloggy.View/Helper');
+
+class CloggyAssetHelperTest extends CakeTestCase {
+	
+	private $__CloggyAsset;
+	private $__base;
+	
+	public function setUp() {
+	
+		parent::setUp();
+			
+		$CakeRequest = new CakeRequest();
+		$CakeResponse = new CakeResponse();
+	
+		$Controller = new Controller($CakeRequest, $CakeResponse);
+		$View = new View($Controller);
+		
+		$this->__CloggyAsset = new CloggyAssetHelper($View);
+		$this->__base = Router::url('/',true).Configure::read('Cloggy.url_prefix').'/'.Configure::read('Cloggy.theme_used');
+	
+	}
+	
+	public function testVendor() {
+		
+		$vendorUrl = $this->__CloggyAsset->getVendorUrl('test');
+		$this->assertEqual($vendorUrl,$this->__base.'/vendors/test');
+		
+		$vendorCssLink = $this->__CloggyAsset->getVendorHtmlTag('test', 'css');
+		$this->assertEqual(htmlentities($vendorCssLink),htmlentities('<link rel="stylesheet" type="text/css" href="/cloggy/default/vendors/test.css" />'));
+		
+		$vendorJsLink = $this->__CloggyAsset->getVendorHtmlTag('test', 'js');
+		$this->assertEqual(htmlentities($vendorJsLink),htmlentities('<script type="text/javascript" src="/cloggy/default/vendors/test.js"></script>'));
+		
+		$vendorUnknownType = $this->__CloggyAsset->getVendorHtmlTag('test', 'tst');
+		$this->assertFalse($vendorUnknownType);
+		
+	}
+	
+	public function testJs() {
+		
+		$jsUrl = $this->__CloggyAsset->getJsUrl('test');
+		$this->assertEqual($jsUrl,$this->__base.'/app/js/test.js');
+		
+		$jsTag = $this->__CloggyAsset->getJsHtmlTag('test');
+		$this->assertEqual($jsTag, '<script type="text/javascript" src="/cloggy/default/app/js/test.js"></script>');
+		
+	}
+	
+	public function testCss() {
+		
+		$cssUrl = $this->__CloggyAsset->getCssUrl('test');
+		$this->assertEqual($cssUrl,$this->__base.'/app/css/test.css');
+		
+		$cssTag = $this->__CloggyAsset->getCssHtmlTag('test');
+		$this->assertEqual(htmlentities($cssTag),htmlentities('<link rel="stylesheet" type="text/css" href="/cloggy/default/app/css/test.css" />'));
+		
+	}
+	
+}
--- View/Helper/CloggyAssetHelper.php
+++ View/Helper/CloggyAssetHelper.php
@@ -2,21 +2,55 @@
 
 App::uses('AppHelper', 'View/Helper');
 
+/**
+ * 
+ * Manage assets
+ * 
+ * @author hiraq
+ * @name CloggyAssetHelper
+ * @package Cloggy
+ * @subpackage Component
+ */
 class CloggyAssetHelper extends AppHelper {
 	
+/**
+ * Helper dependencies
+ * 
+ * @access public
+ * @var array
+ */
 	public $helpers = array('Html');			
 	
+/**
+ * Get webroot/<theme_used>/vendors url
+ * 
+ * @access public
+ * @param string $file
+ * @return string
+ */	
 	public function getVendorUrl($file) {
 		
-		$base = Router::url('/',true).$this->_getBaseUrl();
+		$base = Router::url('/',true).$this->__getBaseUrl();
 		$url = $base.'/vendors/';
 		return $url.$file;	
 		
 	}
 
+/**
+ * Generate html tag by type:
+ * > css : <link>
+ * > js : <script>
+ * 
+ * Get file from webroot/<theme_used>/app
+ * 
+ * @access public
+ * @param string $file
+ * @param string $type
+ * @return boolean|string
+ */	
 	public function getVendorHtmlTag($file,$type) {
 		
-		$base = '/'.$this->_getBaseUrl();
+		$base = '/'.$this->__getBaseUrl();
 		$url = $base.'/vendors/';
 		
 		switch($type) {
@@ -34,40 +68,76 @@ class CloggyAssetHelper extends AppHelper {
 		return false;
 		
 	}
-		
+
+/**
+ * Generate js url from webroot/<theme_used>/app/js
+ * 
+ * @access public
+ * @param string $file
+ * @return string
+ */	
 	public function getJsUrl($file) {
 		
-		$base = Router::url('/',true).$this->_getBaseUrl();
+		$base = Router::url('/',true).$this->__getBaseUrl();
 		$url = $base.'/app/js/';
 		return $url.$file.'.js';
 		
 	}		
 	
+/**
+ * Generate html js tag <script> from webroot/<theme_used>/app/js
+ * 
+ * @access public
+ * @param string $file
+ * @return string
+ */	
 	public function getJsHtmlTag($file) {
 		
-		$base = '/'.$this->_getBaseUrl();
+		$base = '/'.$this->__getBaseUrl();
 		$url = $base.'/app/js/';
 		return $this->Html->script($url.$file);
 		
 	}		
 	
+/**
+ * 
+ * Generate css url from webroot/<theme_used>/app/css
+ * 
+ * @access public
+ * @param string $file
+ * @return string
+ */	
 	public function getCssUrl($file) {
 		
-		$base = Router::url('/',true).$this->_getBaseUrl();
+		$base = Router::url('/',true).$this->__getBaseUrl();
 		$url = $base.'/app/css/';
 		return $url.$file.'.css';
 		
 	}
 	
+/**
+ * 
+ * Generate html <link> for css from webroot/<theme_used>/app/css
+ * 
+ * @access public
+ * @param string $file
+ * @return string
+ */	
 	public function getCssHtmlTag($file) {
 		
-		$base = '/'.$this->_getBaseUrl();
+		$base = '/'.$this->__getBaseUrl();
 		$url = $base.'/app/css/';
 		return $this->Html->css($url.$file);
 		
 	}
 	
-	private function _getBaseUrl() {			
+/**
+ * Get base url based by used theme and url prefix
+ * 
+ * @access private
+ * @return string
+ */	
+	private function __getBaseUrl() {			
 		return Configure::read('Cloggy.url_prefix').'/'.Configure::read('Cloggy.theme_used');
 	}
 	
