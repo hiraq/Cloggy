8f8982c440329a98f6a358e524c3ae8942d78ac8
Hiraq <hrxwan@gmail.com>
2013-01-07 19:28:33 +0700

    unit test model cloggy node media

--- Model/CloggyNodeMedia.php
+++ Model/CloggyNodeMedia.php
@@ -12,4 +12,17 @@ class CloggyNodeMedia extends CloggyAppModel {
 		)
 	);
 	
+	public function saveMedia($nodeId,$type,$location) {
+		
+		$this->create();
+		$this->save(array(
+			'CloggyNodeMedia' => array(
+				'node_id' => $nodeId,
+				'media_file_type' => $type,
+				'media_file_location' => $location
+			)
+		));
+		
+	}
+	
 }
