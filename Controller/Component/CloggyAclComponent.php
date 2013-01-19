1e5b321bda5bc88862d21d4f641da5c7182a6252
Hiraq <hrxwan@gmail.com>
2013-01-19 09:07:09 +0700

    modif cloggy acl component & unit test

--- Controller/Component/CloggyAclComponent.php
+++ Controller/Component/CloggyAclComponent.php
@@ -13,42 +13,36 @@ App::uses('Component', 'Controller');
  *
  */
 class CloggyAclComponent extends Component {
-
+    
     /**
-     * Adapter object
-     * @access private
-     * @var CloggyModuleAcl
+     * AuthComponent object
+     * @var AuthComponent
      */
-    private $__CloggyModuleAcl;
-
+    public $components = array('Auth');
+    
     /**
-     * Controller object as ACO
-     * @access private
-     * @var Controller
+     * CloggyUser model object
+     * @var CloggyUser  
+     */
+    private $__CloggyUser;
+    
+    /**
+     * Requested Controller
+     * @var Controller 
      */
     private $__Controller;
-
+    
     /**
-     * User permission role as ARO
-     * @var string
+     * List of adapters
+     * @var array 
      */
-    private $__userRole;
-
+    private $__adapter = array('Module','Model','Url');
+    
     /**
-     * Flag if user has access or not
-     * @access private
-     * @var boolean
+     * Rules
+     * @var array 
      */
-    private $__hasAccessToModule = false;
-
-    /**
-     * Method name for callback if user doesn't 
-     * have any permission
-     * 
-     * @access private
-     * @var string
-     */
-    private $__callbackFailed;
+    private $__rules;
 
     /**
      * Setup adapter
@@ -58,69 +52,7 @@ class CloggyAclComponent extends Component {
      * @param array $settings
      */
     public function __construct(ComponentCollection $collection, $settings = array()) {
-
-        parent::__construct($collection, $settings);
-        App::uses('CloggyModuleAcl', 'Cloggy.Controller/Component/Acl');
-
-        $this->__CloggyModuleAcl = new CloggyModuleAcl();
-    }
-
-    /**
-     * Auto check each controller for permissions after 'beforeFilter' method called
-     * 
-     * @see Component::startup()
-     * @access public
-     */
-    public function startup(Controller $controller) {
-
-        //merging controller data after beforeFilter method
-        $this->__Controller = $controller;
-
-        if (isset($this->__Controller->cloggyModuleAccess)) {
-
-            $this->__CloggyModuleAcl->setRules($this->__Controller->cloggyModuleAccess);
-
-            /*
-             * only for registered actions
-             */
-            $check = $this->__CloggyModuleAcl->check($this->__userRole, $this->__Controller, $this->__Controller->params['action']);
-            $this->__hasAccessToModule = !is_null($check) ? $check : false;
-
-            if (!$this->__hasAccessToModule && method_exists($this->__Controller, $this->__callbackFailed)) {
-                $action = $this->__callbackFailed;
-                $this->__Controller->$action();
-            }
-        }
-    }
-
-    /**
-     * Set callback method if user doesn't have 
-     * any permission
-     * 
-     * @access public
-     * @param string $action
-     */
-    public function setCallbackIfFailed($action) {
-        $this->__callbackFailed = $action;
-    }
-
-    /**
-     * Set user role as ARO
-     * @access public
-     * @param string $role
-     */
-    public function setUserRole($role) {
-        $this->__userRole = $role;
-    }
-
-    /**
-     * Check if user can access module or not
-     * 
-     * @access public
-     * @return boolean
-     */
-    public function isUserCanAccessModule() {
-        return $this->__hasAccessToModule;
-    }
+        parent::__construct($collection, $settings);        
+    }    
 
 }
