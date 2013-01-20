db74ab251e34f8823975cbc8a16bcd5b57dafe9e
Hiraq <hrxwan@gmail.com>
2013-01-20 11:40:44 +0700

    cloggy user role & perm

---
+++ Module/CloggyUsers/Controller/CloggyUsersRoleController.php
@@ -0,0 +1,37 @@
+<?php
+
+App::uses('CloggyAppController', 'Cloggy.Controller');
+App::uses('Sanitize', 'Utility');
+
+class CloggyUsersRoleController extends CloggyAppController {
+    
+    public $uses = array(
+        'Cloggy.CloggyUser', 
+        'Cloggy.CloggyUserRole',
+        'Cloggy.CloggyValidation'
+    );        
+
+    public function beforeFilter() {
+
+        parent::beforeFilter();
+        $this->Auth->deny('*');
+
+        //load additional helpers        
+        $this->helpers[] = 'Form';
+
+        //setup pagination
+        $this->paginate = array(
+            'CloggyUserRole' => array(
+                'limit' => 10,
+                'contain' => false,
+                'order' => array('role_name' => 'asc')
+            )
+        );
+        
+    }
+    
+    public function index() {
+        
+    }
+    
+}
