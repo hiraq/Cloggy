29ba9fe3562fdf86eb942639d78d1972857aceb6
Hiraq <hrxwan@gmail.com>
2013-02-02 08:30:50 +0700

    cloggy app model set tablePrefix

--- Model/CloggyAppModel.php
+++ Model/CloggyAppModel.php
@@ -3,6 +3,7 @@
 class CloggyAppModel extends AppModel {
 
     public $actsAs = array('Containable');
-    public $cacheQueries = true;        
+    public $cacheQueries = true;      
+    public $tablePrefix = 'cloggy_';
 
 }
