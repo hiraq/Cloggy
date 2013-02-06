84040d9061b816e5a3b163f278d0781e5b166c39
Hiraq <hrxwan@gmail.com>
2013-02-06 08:06:09 +0700

    all cloggy unit test

--- Test/Case/AllCloggyTest.php
+++ Test/Case/AllCloggyTest.php
@@ -1,17 +1,34 @@
 <?php
 
-require_once dirname(__FILE__) . DS . 'CloggyGroupTestCase.php';
+//require_once dirname(__FILE__) . DS . 'CloggyGroupTestCase.php';
+//
+///**
+// * @link https://github.com/cakephp/debug_kit/blob/master/Test/Case/AllDebugKitTest.php follow DebugKit group test case
+// */
+//class AllCloggyTest extends CloggyGroupTestCase {
+//
+//	public static function suite() {
+//		$suite = new self;
+//		$files = $suite->getTestFiles();
+//		$suite->addTestFiles($files);
+//
+//		return $suite;
+//	}
+//}
 
-/**
- * @link https://github.com/cakephp/debug_kit/blob/master/Test/Case/AllDebugKitTest.php follow DebugKit group test case
- */
-class AllCloggyTest extends CloggyGroupTestCase {
+class AllCloggyTest extends CakeTestSuite {
 
-	public static function suite() {
-		$suite = new self;
-		$files = $suite->getTestFiles();
-		$suite->addTestFiles($files);
+    public static function suite() {
+        
+        $suite = new CakeTestSuite('All Cloggy tests');
+        
+        /*
+         * components
+         */
+        $suite->addTestDirectory(APP . 'Plugin' . DS . 'Cloggy' . DS .
+                'Test' . DS . 'Case');                
+                
+        return $suite;
+    }           
 
-		return $suite;
-	}
 }
