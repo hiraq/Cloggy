e70fa7e22743a840b36e1869d0731ac4e7d2416a
Hiraq <hrxwan@gmail.com>
2013-02-06 08:15:24 +0700

    fix all cloggy unit test

--- Test/Case/AllCloggyTest.php
+++ Test/Case/AllCloggyTest.php
@@ -1,21 +1,5 @@
 <?php
 
-//require_once dirname(__FILE__) . DS . 'CloggyGroupTestCase.php';
-//
-///**
-// * @link https://github.com/cakephp/debug_kit/blob/master/Test/Case/AllDebugKitTest.php follow DebugKit group test case
-// */
-//class AllCloggyTest extends CloggyGroupTestCase {
-//
-//	public static function suite() {
-//		$suite = new self;
-//		$files = $suite->getTestFiles();
-//		$suite->addTestFiles($files);
-//
-//		return $suite;
-//	}
-//}
-
 class AllCloggyTest extends CakeTestSuite {
 
     public static function suite() {
