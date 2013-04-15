25f013bfb3aed05dce7d437d9a5f5c25d9770fed
Hiraq <hrxwan@gmail.com>
2013-04-15 22:55:15 +0700

    cloggy search models & mysql controller

--- Module/CloggySearch/Controller/CloggySearchMysqlController.php
+++ Module/CloggySearch/Controller/CloggySearchMysqlController.php
@@ -1,7 +1,19 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+App::uses('CloggyAppController', 'Cloggy.Controller');
+
+class CloggySearchMysqlController extends CloggyAppController {
+    
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+    
+    public function index() {
+        //pass
+    }
+    
+    public function manage() {
+        $this->set('title_for_layout',__d('cloggy','Cloggy Search Management - MysqlFullText Search Engine'));
+    }
+    
+}
